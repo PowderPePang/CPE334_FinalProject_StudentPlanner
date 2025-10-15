38f45fc72c09b051b472ff031ec04fedc1e1350d
Pemika Chongkwanyuen <paeng.pemika@gmail.com>
2025-10-15 21:02:25 +0700

    Add github actions

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [react()],
+
+    // path repository (github pages)
+    base: "/CPE334_FinalProject_StudentPlanner/",
+});
